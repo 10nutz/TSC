783fa12f751ff364efd23ef7db21ed10e21fc5af
Ionut-Alexandru Oprea <91005312+10nutz@users.noreply.github.com>
2024-03-27 10:42:30 +0200

    Added multiple test cases.

--- lab2/tools/regression.bat
+++ lab2/tools/regression.bat
@@ -1,2 +1,9 @@
-call .\run_test.bat 10 10 2 2 case_rnd_rnd console
 call .\run_test.bat 10 10 0 0 case_inc_inc console
+call .\run_test.bat 10 10 0 1 case_inc_dec console
+call .\run_test.bat 10 10 0 2 case_inc_rnd console
+call .\run_test.bat 10 10 1 0 case_dec_inc console
+call .\run_test.bat 10 10 1 1 case_dec_dec console
+call .\run_test.bat 10 10 1 2 case_dec_rnd console
+call .\run_test.bat 10 10 2 0 case_rnd_inc console
+call .\run_test.bat 10 10 2 1 case_rnd_dec console
+call .\run_test.bat 10 10 2 2 case_rnd_rnd console
